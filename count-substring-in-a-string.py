6a38cb61c4b7cc72e0eaa8bff5d843a1523be69a
kumarprd <40861429+kumarprd@users.noreply.github.com>
2018-07-25 00:51:17 +0530

    Update count-substring-in-a-string.py

--- count-substring-in-a-string.py
+++ count-substring-in-a-string.py
@@ -1,20 +1,20 @@
 def count_substring(string, sub_string):
-    count = 0
-    for i in range(0,len(string)):
-        substr = string[i:i+len(sub_string)]
-        
-        if len(substr) == len(sub_string):
-            #print i, substr
-            if substr == sub_string:
-                #print i, substr
-                count += 1
-     
-    return count
-       
-        
- if __name__ == '__main__':
-    string = raw_input().strip()
-    sub_string = raw_input().strip()
-    
-    count = count_substring(string, sub_string)
-    print count
+   count = 0
+   for i in range(0,len(string)):
+       substr = string[i:i+len(sub_string)]
+
+       if len(substr) == len(sub_string):
+           #print i, substr
+           if substr == sub_string:
+               #print i, substr
+               count += 1
+
+   return count
+
+
+if __name__ == '__main__':
+   string = raw_input("String:").strip()
+   sub_string = raw_input("Substring:").strip()
+
+   count = count_substring(string, sub_string)
+   print count
